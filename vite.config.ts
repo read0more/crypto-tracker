b2feede9117585b1f372963aada5e08a9c96914d
yk0816 <read0more@gmail.com>
2023-02-22 16:54:11 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
