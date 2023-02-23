68f66a69c76d4f5e58a7f50ebac6225a3625cae4
read0more <read0more@gmail.com>
2023-02-23 22:32:49 +0900

    :sparkles: get coin list from coinpaprika

--- src/components/tests/CoinList.test.tsx
+++ src/components/tests/CoinList.test.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import CoinList from '@/components/CoinList';
+import fakeList from '@/testDoubles/fakes/fakeList.json';
 
-// const queryClient = new QueryClient();
-// function Wrapper({ children }: { children: JSX.Element }) {
-//   return (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// }
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+function Wrapper({ children }: { children: JSX.Element }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
 
-it('API로 받은 코인 데이터 렌더링', () => {
-  const { getByText } = render(<CoinList />);
-  getByText('CoinList');
+it('API로 받은 코인 데이터 렌더링', async () => {
+  const { findByText } = render(
+    <Wrapper>
+      <CoinList />
+    </Wrapper>
+  );
+
+  for (const { id } of fakeList) {
+    findByText(id);
+  }
 });
--- src/testDoubles/mocks/handlers.ts
+++ src/testDoubles/mocks/handlers.ts
@@ -2,5 +2,7 @@ import { rest } from 'msw';
 import fakeList from '@/testDoubles/fakes/fakeList.json';
 
 export default [
-  rest.get('https://API주소', (_, res, ctx) => res(ctx.json(fakeList))),
+  rest.get('https://api.coinpaprika.com/v1/coins', (_, res, ctx) =>
+    res(ctx.json(fakeList))
+  ),
 ];
